eee4a7f962643a1acbbeff33ea6bb1381110bd4b
mb11jr <bentaleb11jr@gmail.com>
2025-11-24 10:44:58 +0100

    megaphone exercise: done
    still need to fix the .gitignore file

---
+++ ex00/src/megaphone.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+int main(int ac, char **av)
+{
+	if (ac == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	}
+	for (int i = 1; i < ac; i++)
+	{
+		for (int j = 0; av[i][j]; j++)
+			std::cout << (char)std::toupper(av[i][j]);
+		if (i < ac - 1)
+			std::cout << " ";
+	}
+	std::cout << std::endl;
+}
